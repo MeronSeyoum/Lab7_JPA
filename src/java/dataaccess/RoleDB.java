db5ba6e9be8bedaea6b86fe2d63d1958d9c967f4
Meron Seyoum <meryato@gmail.com>
2022-03-25 23:13:31 -0600

    DBUser and DBRole class are modified to be compatible with entity class

--- src/java/dataaccess/RoleDB.java
+++ src/java/dataaccess/RoleDB.java
@@ -6,6 +6,8 @@ package dataaccess;
 
 
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import models.Role;
 
 /**
@@ -15,6 +17,13 @@ import models.Role;
 public class RoleDB {
     
  public List<Role> getAll() throws Exception {
-        
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+
+        try {
+            Query query = em.createNamedQuery("Role.findAll");
+            return query.getResultList();
+        } finally {
+            em.close();
+        } 
     }  
 }
--- src/java/dataaccess/UserDB.java
+++ src/java/dataaccess/UserDB.java
@@ -4,7 +4,12 @@
  */
 package dataaccess;
 
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.*;
 import models.Role;
 import models.User;
 
@@ -15,18 +20,72 @@ import models.User;
 public class UserDB {
 
     public List<User> getAll() throws Exception {
-       
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+
+        try {
+            Query query = em.createNamedQuery("User.findAll");
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
     }
 
     public User get(String email) throws Exception {
        
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+
+        try {
+            User user = em.find(User.class, email);
+            return user;
+        } finally {
+            em.close();
+        }
+    }
+
+    public void insert(User user) throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+            trans.begin();
+            em.persist(user);
+            em.merge(user);
+            trans.commit();  
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+            em.close();
+        }
     }
 
     public void update(User user) throws Exception {
-      
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+
+        try {
+            trans.begin();
+            em.persist(user);
+            em.merge(user);
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+            em.close();
+        }
     }
 
     public void delete(User user) throws Exception {
-       
+       EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        EntityTransaction trans = em.getTransaction();
+        
+        try {
+            trans.begin();
+            em.remove(em.merge(user));
+            trans.commit();
+        } catch (Exception ex) {
+            trans.rollback();
+        } finally {
+            em.close();
+        }
     }
 }
